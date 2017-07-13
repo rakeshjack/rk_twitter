a02f6e65a98d9a0b19ead1b8d11316bf82f3ecf0
Rakesh <rakeshjack7@gmail.com>
2017-07-13 13:03:48 +0530

    Commit 1 Copycat of Twitter Practising Laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        Twitter
+                        RK Twitter
                     </a>
                 </div>
 
